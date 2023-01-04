609e0bd1bc3cabeaac4b3db4e4cab5de8c3bcf45
ashok <anumulaashok85@gmail.com>
2023-01-04 17:14:43 +0530

    hello world removed

--- src/main/java/com/mongoDemo/demo/MongoDemoApplication.java
+++ src/main/java/com/mongoDemo/demo/MongoDemoApplication.java
@@ -8,7 +8,6 @@ public class MongoDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MongoDemoApplication.class, args);
-        System.out.println("hello");
     }
 
 
