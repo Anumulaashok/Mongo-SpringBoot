a84d4a9b22c553420f502bc2ea2e2e25a9eb1227
ashok <anumulaashok85@gmail.com>
2023-01-04 16:55:00 +0530

    Hello world word added

--- src/main/java/com/mongoDemo/demo/MongoDemoApplication.java
+++ src/main/java/com/mongoDemo/demo/MongoDemoApplication.java
@@ -8,6 +8,7 @@ public class MongoDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MongoDemoApplication.class, args);
+        System.out.println("hello");
     }
 
 
